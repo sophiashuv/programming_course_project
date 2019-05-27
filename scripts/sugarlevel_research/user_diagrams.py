79320bede2d10efe774e58f48e7d4192077ca191
sophiashuv <45092554+sophiashuv@users.noreply.github.com>
2019-05-27 12:26:19 +0300

    Update user_diagrams.py

--- scripts/sugarlevel_research/user_diagrams.py
+++ scripts/sugarlevel_research/user_diagrams.py
@@ -4,14 +4,20 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def read_file(filename):
+def file_name(login):
     """
-    The function that reads json file
+    returns a file name
     """
-    file = codecs.open(filename, 'r', 'utf-8-sig')
-    lines = json.load(file)
-    file.close()
-    return lines
+    if " " in login:
+        login = login.replace(" ", "")
+    return str(login).lower() + '.json'
+
+
+def user_json(login):
+    """
+    returns the contents of json
+    """
+    return (json.load(codecs.open(file_name(login), 'r', 'utf-8-sig')))["events"]
 
 
 def parameters(date, lines):
@@ -27,42 +33,69 @@ def parameters(date, lines):
     return height, bars, date
 
 
-def count_mid(lines):
+def count_mid(date, login):
     """
-    The funcrion tat counts medium level of sugar of a day and returns a list of medium levels of sugar for
+    Returns medium value of sugal level of a day
+    """
+    lst_doz = []
+    user_file = user_json(login)
+    for day in user_file:
+        if day['date'] == date:
+            lst_doz.append(round(float(day["value"]), 2))
+    return sum(lst_doz)/len(lst_doz)
+
+
+def count_mid_week(login):
+    """
+    Counts medium level of sugar of a day and returns a list of medium levels of sugar for
     the last 7 days and list od dates
     """
-    n = -1
-    lst_sug = []
-    lst_date = []
-    while n != -36:
-        height, bars, date = parameters(lines['events'][n]['date'], lines)
-        mid = sum(height)/len(height)
-        n -= 5
-        lst_sug.append(mid)
-        lst_date.append(date)
-    return lst_sug, lst_date
+    user_file = user_json(login)
+    lst_dozes = []
+    lst_dates = []
+    for i in user_file:
+        if i['date'] not in lst_dates:
+            lst_dates.append(i['date'])
+    for i in lst_dates[-7:]:
+        lst_dozes.append(count_mid(i, login))
+    return lst_dozes,  lst_dates[-7:]
 
 
-def d1(height, bars, date):
+def day(login):
     """
-    The function that builds a Stacked Barplot
+    Returns lists of heights and bars needed for a diagram and a day
     """
+    height, bars = [], []
+    user_file = (json.load(codecs.open(file_name(login), 'r', 'utf-8-sig')))["events"]
+    for days in user_file:
+        if days["date"] == user_file[-1]["date"]:
+            height.append(float(days["value"]))
+            bars. append(days["time"])
+    return height, bars, user_file[-1]["date"]
+
+
+def daily_diagram(login):
+    """
+    Builds a diagram of daily level of sugar
+    """
+    height, bars, date = day(login)
     barWidth = 0.25
     y_pos = np.arange(len(bars))
     plt.bar(y_pos, height, color='#EB984E', width=barWidth, edgecolor='white', label='var1')
     plt.xticks(y_pos, bars, color='#873600')
     plt.yticks(color='#873600')
     plt.xlabel(date, fontweight='bold', color='#D35400')
-    plt.show()
+    plt.savefig('daydiagram.png')
+    plt.close()
 
 
-def d2(size, names):
+def weekly_diagram(login):
     """
-    The function that builds a Pie plot
+    Builds a diagram of weekle sugar level values
     """
+    size, names = count_mid_week(login)
     np = []
-    for i in range(7):
+    for i in range(min(7, len(size))):
         np.append((str(round(size[i], 2)) + "\n"+names[i]))
     fig = plt.figure()
     fig.patch.set_facecolor('white')
@@ -72,13 +105,10 @@ def d2(size, names):
     plt.pie(size, labels=np, colors=col, shadow=True, startangle=140)
     p = plt.gcf()
     p.gca().add_artist(my_circle)
-    plt.show()
+    plt.savefig('weekdiagram.png')
+    plt.close()
 
 
 if __name__ == "__main__":
-    height, bars, date = parameters("11.05.19", read_file("name_surname.json"))
-    d1(height, bars, date)
-    size, names = count_mid(read_file("name_surname.json"))
-    d2(size, names)
-
-
+    daily_diagram("name_surname.json")
+    weekly_diagram("name_surname.json")
